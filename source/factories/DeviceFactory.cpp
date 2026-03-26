35a63b6c5308e2c22959a4245362dfa5af7c135a
WarperSan <leumas.ecole@gmail.com>
2026-03-26 18:44:01 -0400

    Implemented `DeviceFactory::loadFromJson()`

--- source/factories/DeviceFactory.cpp
+++ source/factories/DeviceFactory.cpp
@@ -12,7 +12,20 @@ std::unordered_map<int, Device*> DeviceFactory::loadFromJson(const nlohmann::bas
 	if (!obj.contains(DEVICES_KEY))
 		throw std::invalid_argument("The property '" + std::string(DEVICES_KEY) + "' does not exist.");
 
-	const auto devicesProp = obj.at(DEVICES_KEY);
+	const auto& devicesProp = obj.at(DEVICES_KEY);
 
-	return {};
+	if (!devicesProp.is_array())
+		throw std::invalid_argument("The property '" + std::string(DEVICES_KEY) + "' must be an array.");
+
+	std::unordered_map<int, Device*> devices;
+
+	for (const auto& deviceItem : devicesProp.items())
+	{
+		auto device = loadDevice(deviceItem.value());
+		const auto id = device.getId();
+
+		devices.insert({id, &device});
+	}
+
+	return devices;
 }
