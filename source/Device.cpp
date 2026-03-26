da2388a7da013c0d179065c3285c4cdcbe8721ea
WarperSan <leumas.ecole@gmail.com>
2026-03-26 18:21:46 -0400

    Added missing references

---
+++ source/Device.cpp
@@ -0,0 +1,14 @@
+#include "../Device.h"
+
+Device::Device(int id, const std::string& type, const std::string& name, int lockTime)
+{
+}
+
+std::unordered_map<int, Device*> Device::BuildDevicesFromJsonFile(const std::string& path)
+{
+	std::unordered_map<int, Device*> result;
+	
+	
+	
+	return result;
+}
