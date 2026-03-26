ad33a696cb532a134eb27aad4bbb03aa0fc9a856
WarperSan <leumas.ecole@gmail.com>
2026-03-26 19:35:57 -0400

    Added `TeamFactory::loadTeam()`

--- source/factories/TeamFactory.cpp
+++ source/factories/TeamFactory.cpp
@@ -2,7 +2,35 @@
 
 Team TeamFactory::loadTeam(const nlohmann::basic_json<>& obj)
 {
-	return {"", {}, 0};
+	constexpr auto NAME_KEY = "name";
+	constexpr auto CAPABILITIES_KEY = "capabilities";
+	constexpr auto MAX_CAPACITY_KEY = "max_capacity";
+
+	if (!obj.contains(NAME_KEY))
+		throw std::invalid_argument("The property '" + std::string(NAME_KEY) + "' does not exist.");
+
+	if (!obj.contains(CAPABILITIES_KEY))
+		throw std::invalid_argument("The property '" + std::string(CAPABILITIES_KEY) + "' does not exist.");
+
+	const auto& capabilitiesProp = obj.at(CAPABILITIES_KEY);
+
+	if (!capabilitiesProp.is_array())
+		throw std::invalid_argument("The property '" + std::string(CAPABILITIES_KEY) + "' must be an array.");
+
+	auto name = obj.at(NAME_KEY).get<std::string>();
+	std::vector<std::string> capabilities = {};
+	int maxCapacity = -1;
+
+	for (const auto& capabilityItem : capabilitiesProp.items())
+	{
+		auto capability = capabilityItem.value().get<std::string>();
+		capabilities.emplace_back(capability);
+	}
+
+	if (obj.contains(MAX_CAPACITY_KEY))
+		maxCapacity = obj.at(MAX_CAPACITY_KEY).get<int>();
+
+	return {name, capabilities, maxCapacity};
 }
 
 std::vector<Team*> TeamFactory::loadFromJson(const nlohmann::basic_json<>& obj)
