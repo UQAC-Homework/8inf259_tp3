bc02422bfc5af92f6df8273274d780c883a59a6f
WarperSan <leumas.ecole@gmail.com>
2026-04-02 20:17:01 -0400

    Updated `library::string::join()`

--- include/library/string.h
+++ include/library/string.h
@@ -1,27 +1,25 @@
 #ifndef INC_8INF259_TP3_STRING_H
 #define INC_8INF259_TP3_STRING_H
+#include <sstream>
 #include <string>
 #include <vector>
 
 namespace library::string
 {
-	// Modified from: https://www.oreilly.com/library/view/c-cookbook/0596007612/ch04s09.html
-	inline std::string join(
-		const std::vector<std::string>& v,
-		const std::string& delimiter
-	)
+	template <typename T>
+	std::string join(const std::vector<T>& v, const std::string& delimiter)
 	{
-		std::string s;
+		std::ostringstream oss;
 
-		for (auto p = v.begin(); p != v.end(); ++p)
+		for (auto it = v.begin(); it != v.end(); ++it)
 		{
-			s += *p;
+			oss << *it;
 
-			if (p != v.end() - 1)
-				s += delimiter;
+			if (std::next(it) != v.end())
+				oss << delimiter;
 		}
 
-		return s;
+		return oss.str();
 	}
 }
 
