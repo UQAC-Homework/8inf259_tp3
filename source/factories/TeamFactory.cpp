d940dff88ce73029ef0f820fe27d39f2fe597d0c
WarperSan <leumas.ecole@gmail.com>
2026-04-09 14:45:27 -0400

    TeamFactory::loadTeam

--- source/factories/TeamFactory.cpp
+++ source/factories/TeamFactory.cpp
@@ -2,27 +2,22 @@
 
 Team TeamFactory::loadTeam(const nlohmann::basic_json<>& obj)
 {
-	constexpr auto NAME_KEY = "name";
 	constexpr auto CAPABILITIES_KEY = "capabilities";
-	constexpr auto MAX_CAPACITY_KEY = "max_capacity";
+
+	auto name = obj.at("name").get<std::string>();
+	std::vector<std::string> capabilities = {};
+	auto maxCapacity = obj.value<int>("max_capacity", NO_LIMIT_CAPACITY);
 
 	const auto& capabilitiesProp = obj.at(CAPABILITIES_KEY);
 
 	if (!capabilitiesProp.is_array())
 		throw std::invalid_argument("The property '" + std::string(CAPABILITIES_KEY) + "' must be an array.");
 
-	auto name = obj.at(NAME_KEY).get<std::string>();
-	std::vector<std::string> capabilities = {};
-	int maxCapacity = NO_LIMIT_CAPACITY;
-
 	for (const auto& capabilityItem : capabilitiesProp.items())
 	{
 		auto capability = capabilityItem.value().get<std::string>();
 		capabilities.emplace_back(capability);
 	}
-
-	if (obj.contains(MAX_CAPACITY_KEY))
-		maxCapacity = obj.at(MAX_CAPACITY_KEY).get<int>();
 
 	return {name, capabilities, maxCapacity};
 }
